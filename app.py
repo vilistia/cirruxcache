4cedd710e2030099a1d75021bb2a1e095dfb4334
Samuel Alba <sam.alba@gmail.com>
2010-07-21 19:18:10 +0200

    fix data forwarding

---
+++ app.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# CirruxCache provides dynamic HTTP caching on AppEngine (CDN like)
+# Copyright (C) 2009 Samuel Alba <sam.alba@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+"""CirruxCache provides dynamic HTTP caching on AppEngine (CDN like)
+
+http://cirrux.org/cache/
+http://code.google.com/p/cirruxcache/
+"""
+
+__version__ = 'trunk'
+__author__ = [
+    'Samuel Alba <sam.alba@gmail.com>'
+]
+__license__ = 'GNU Public License version 2'
+
+import sys, os
+root = os.path.dirname(os.environ['PATH_TRANSLATED'])
+sys.path.append(os.path.join(root, 'lib'))
+sys.path.append(os.path.join(root, 'contrib'))
+
+import logging
+import web
+
+from services.cron import Cron
+from services.admin import Admin
+from services.store import Store
+from services.debug import Debug
+from lib import cache, redirect, forward
+
+
+# URL mapping
+urls = {}
+
+urls['default'] = (
+		'(/debug/.*)', 'Debug',
+		'/_admin/(.*)', 'Admin',
+		'/_store/(.*)', 'Store',
+		'(/data/.*)', 'Static',
+		'/www(/.*)', 'Www',
+		'/_cron/(.*)', 'Cron',
+		'/(.*)', 'Root'
+		)
+
+# POP definition
+# You can define and configure your Point Of Presence
+
+class Static(cache.Service):
+	origin = 'http://static.mydomain.tld'
+	maxTTL = 2592000 # 1 month
+	ignoreQueryString = True
+
+class Www(cache.Service):
+	origin = 'http://www.mydomain.tld'
+	allowFlushFrom = ['127.0.0.1']
+	forceTTL = 3600 # 1 hour
+	ignoreQueryString = True
+	forwardPost = False
+
+# !POP
+
+# Dynamic configuration
+# disabled for performance issue
+#def initServices(urls):
+#	cfg = config.Config()
+#	for service, meta in cfg.all():
+#		if service[0] != '_':
+#			continue
+#		globals()[service] = type(service, (Service,), meta)
+#		urls = ('(%s/.*)' % meta['mount'], service) + urls
+#	return urls
+
+class Root(object):
+
+	def GET(self, request):
+		web.header('Content-Type', 'text/plain')
+		content = 'CirruxCache (%s) / http://code.google.com/p/cirruxcache/\n' % __version__
+		if request:
+			raise web.HTTPError(status='404 Not Found', data=content)
+		return content
+
+class VhostMapper(object):
+
+	def __iter__(self):
+		url = ('/(.*)', 'Root')
+		if 'HTTP_HOST' in web.ctx.environ:
+			vhost = web.ctx.environ['HTTP_HOST']
+			if vhost in urls:
+				url = urls[vhost]
+			elif 'default' in urls:
+				url = urls['default']
+		return iter(url)
+
+if __name__ == '__main__':
+#	urls = initServices(urls)
+#	debug = ('SERVER_SOFTWARE' in os.environ and os.environ['SERVER_SOFTWARE'] == 'Development/1.0')
+#	if debug:
+#	from services.debug import Debug
+#	urls = ('(/debug/.*)', 'Debug') + urls
+#	logging.getLogger().setLevel(logging.DEBUG)
+	app = web.application(VhostMapper(), globals())
+	app.cgirun()
---
+++ static/admin.js
@@ -0,0 +1,122 @@
+/*
+ * This file is part of the CirruxCache project
+ * http://code.google.com/p/cirruxcache/
+ */
+
+$(document).ready(function() {
+		$("#flush > span").hide();
+		var t = $("#tabs");
+		var tabCallback = [null, fetchStore, fetchStats];
+		t.tabs({ show: function(event, ui) {
+			var cb = tabCallback[t.tabs("option", "selected")];
+			if (cb)
+				cb();
+			$("div > span:first-child").hide();
+			}});
+		$("#flush > input").bind("click", flush);
+		$("#store > fieldset > form").bind("change", newStore);
+		$("#store > fieldset > input[type=text]").bind("blur", checkStorePath);
+		$("#stats > input").bind("click", function() { fetchStats(true) });
+		});
+
+var showMessage = function(target, message) {
+	var t = $(target);
+	t.html(message);
+	t.slideDown("fast");
+	setTimeout(function() { t.slideUp("fast") }, 3000);
+}
+
+var flush = function() {
+	var data = $("#flush > textarea")[0].value.split("\n");
+	for (var i = 0; i < data.length; ++i)
+	{
+		var req = jQuery.trim(data[i]);
+		if (req[0] != "/")
+			req = "/" + req;
+		$.ajax({type: "DELETE", url: req, dataType: "text"});
+	}
+	var message = data.length + " flush request" + (data.length > 1 ? "s" : "") + " sent";
+	showMessage("#flush > span", message);
+}
+
+var fetchStore = function(force) {
+	var target = $("#store > ul");
+	if (!force && target.html())
+		return;
+	$.ajax({
+		url: document.location.pathname + "store",
+		dataType: "text",
+		success: function(data, textStatus, XMLHttpRequest) {
+			if (!data)
+				data = " ";
+			target.html(data);
+			}
+		});
+}
+
+var checkStorePath = function() {
+	var url = $("#store > fieldset > input[type=text]")[0];
+	if (url.value[url.value.length - 1] == "/" || url.value[0] != "/") {
+		alert("Bad path syntax");
+		url.focus();
+		return false;
+	}
+	return true;
+}
+
+var newStore = function() {
+	if (!checkStorePath())
+		return false;
+	var url = $("#store > fieldset > input[type=text]")[0].value;
+	url += "/new";
+	$.ajax({
+		type: "GET",
+		url: url,
+		dataType: "text",
+		success: addStore,
+		error: function(XMLHttpRequest, textStatus, errorThrown) {
+			var message = textStatus + " (" + XMLHttpRequest.status + " " + XMLHttpRequest.statusText + ")";
+			showMessage("#store > span", message);
+			}
+		});
+}
+
+var addStore = function(data, textStatus, XMLHttpRequest) {
+	var form = $("#store > fieldset > form");
+	form.ajaxSubmit({
+		url: data,
+		dataType: "text",
+		success: function() {
+			fetchStore(true);
+			form[0].reset();
+			showMessage("#store > span", "Upload successful");
+			}
+		});
+}
+
+var delStore = function(url) {
+	var c = confirm("Are you sure to delete this file?");
+	if (!c)
+		return;
+	$.ajax({
+		type: "GET",
+		url: url + "/delete",
+		dataType: "text",
+		complete: function() { fetchStore(true); }
+	});
+}
+
+var fetchStats = function(force) {
+	var target = $("#stats > ul");
+	if (!force && target.html())
+		return;
+	$.ajax({
+		url: document.location.pathname + "stats",
+		dataType: "text",
+		success: function(data, textStatus, XMLHttpRequest) {
+			if (!data)
+				data = " ";
+			target.html(data);
+			}
+		});
+}
